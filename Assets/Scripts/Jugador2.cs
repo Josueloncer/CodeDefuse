9b00acb2d3b4105aa710a30e1ed2d9571791f3cf
Josueloncer <44252254+Josueloncer@users.noreply.github.com>
2019-02-13 17:34:55 -0600

    Revert "Se agrego el multijugador local"

---
+++ Assets/Scripts/Jugador2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador2 : MonoBehaviour {
+
+    Material m_Material;
+
+    void Start()
+    {
+        //Fetch the Material from the Renderer of the GameObject
+        m_Material = GetComponent<Renderer>().material;
+        print("Materials " + Resources.FindObjectsOfTypeAll(typeof(Material)).Length);
+        m_Material.color = Color.blue;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("right shift") == true)
+        {
+            if(m_Material.color == Color.blue)
+            {
+                m_Material.color = Color.yellow;
+            }
+
+            else
+            {
+                m_Material.color = Color.blue;
+            }
+        }
+    }
+}
