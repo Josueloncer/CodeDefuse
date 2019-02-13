a9fa63fe02557e2b42d8b44e4fc6e3fa2065f9c9
Yo030 <seryrak49@gmail.com>
2019-02-12 18:14:05 -0600

    Se agrego el multijugador local

--- Assets/Scripts/Musica.cs
+++ Assets/Scripts/Musica.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Musica : MonoBehaviour {
 
     public AudioClip musica;
+    public GameObject mus;
+    AudioSource muss;
 
     private void Start()
     {
-        //musica = GetComponent<AudioSource>();
+        //muss = GetComponent<AudioSource>();
+       // muss.
+        
+        Scene EcenaActual = SceneManager.GetActiveScene();
+        int EA = SceneManager.GetActiveScene().buildIndex;
+
+        switch (EA)
+        {
+
+            case 0:
+
+                break;
+
+
+        }
+
+
     }
 
     // Update is called once per frame
