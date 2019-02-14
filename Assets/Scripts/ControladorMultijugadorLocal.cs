40bd0378e56a8db4f59149a822349a5b90e1f56e
Yo030 <seryrak49@gmail.com>
2019-02-13 23:58:01 -0600

    Multijugador Local

---
+++ Assets/Scripts/ControladorMultijugadorLocal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorMultijugadorLocal : MonoBehaviour {
+    
+
+    public void NivelFacil()
+    {
+        SceneManager.LoadScene("MultiLocalFacil");
+    }
+
+    public void NivelNormal()
+    {
+        SceneManager.LoadScene("MultiLocalNormal");
+    }
+
+    public void NivelDificil()
+    {
+        SceneManager.LoadScene("MultiLocalDificil");
+    }
+
+    public void NivelExperto()
+    {
+        SceneManager.LoadScene("MultiLocalExperto");
+    }
+
+    public void Regresar()
+    {
+        //SceneManager.LoadScene("");                               //REGRESAR A LA ESCENA PASADA **MAIN MENU**
+    }
+
+}
