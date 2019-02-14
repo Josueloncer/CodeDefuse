8ebdcfba4c9e8c3d53ae811690be7555ad0f721e
vikarfd <pikazoide_7@hotmail.com>
2019-02-13 23:31:44 -0600

    Ligar escenas de single player a el menu. Creacion de multiplayer online demo.

---
+++ Assets/Victor López/VScripts/Individual_networking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Individual_networking : NetworkBehaviour{
+
+
+	public GameObject player;
+	//public GameObject[] characterModel;
+
+	public override void OnStartLocalPlayer(){
+		GetComponent<Movement_OnlinePlayerPrueba>().enabled = true;
+		player.SetActive(true);
+
+		/*foreach(GameObject go in characterModel){
+			go.SetActive(false);
+		}*/
+	}
+}
