94694bc693bdda1b40825a735162206b792eba58
vikarfd <pikazoide_7@hotmail.com>
2019-02-28 17:32:40 -0600

    Se implemento un menu de Online Multiplayer que armonice con el que ya se tenia. Se agregaron botones para salir del gameplay de cualquier modo y cualquier dificultad.

---
+++ Assets/Victor López/VScripts/OffUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffUtilities : MonoBehaviour {
+	public static bool OnlineOn = false;
+
+
+	public void goTrue(){
+		OnlineOn = true;
+	}
+}
